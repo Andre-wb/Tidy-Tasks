a4a0b67a45a7c3315ac557237358c5e0ed76bf8f
andreykaravaev640 <andreykaravaev640@gmail.com>
2024-12-03 21:54:22 +0800

    доработка header

--- components/script.js
+++ components/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const headerMenuButton = document.querySelector('.header-menu');
     const nav = document.querySelector('nav');
+    const main = document.querySelector('main');
     const topLine = document.getElementById('top_line');
     const centerLine = document.getElementById('center_line');
     const bottomLine = document.getElementById('bottom_line');
@@ -9,8 +10,9 @@ document.addEventListener('DOMContentLoaded', () => {
         headerMenuButton.addEventListener('click', () => {
             nav.classList.toggle('active');
             topLine.classList.toggle('active');
-            centerLine.classList.toggle('active'); // Прячем среднюю линию
+            centerLine.classList.toggle('active');
             bottomLine.classList.toggle('active');
+            main.classList.toggle('active');
         });
     }
 });
